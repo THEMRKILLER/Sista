4f5a169bb45e9aec757ad0d9d0d160ed3d5b51ed
leyven <nyhedgg@gmail.com>
2016-12-20 12:32:04 -0600

    modelos Cita y Tipo.
    aun se necesitan probar los metodos

--- app/cita.php
+++ app/cita.php
@@ -8,36 +8,100 @@ class cita extends Model
 {
    	 protected $table = 'cita';
      protected $fillable = [
-         'tipo_id','fecha', 'hora', 'cliente_nombre','cliente_telefono','cliente_email',
+         'tipo_id','calendario_id','fecha', 'hora', 'cliente_nombre','cliente_telefono','cliente_email',
     ];
     //relaciones
-      public function Tipo()
+      public function tipo()
     	{
        return $this->hasOne('App\tipo');
     	}
     //metodos de clase
-      public function crear()
+
+    /* 
+    * @param arrayDatos estructura con nombre,telefono,email,fecha y hora de la cita
+    */
+      public function crear($arrayDatos,$calendario_id,$tipo_id)
     	{
-      
+          
+            $NuevaCita = new App\cita;
+            $NuevaCita->calendario_id =$calendario_id;
+            $NuevaCita->tipo_id =$tipo_id;
+            $NuevaCita->fecha =$arrayDatos['fecha'];
+            $NuevaCita->hora =$arrayDatos['hora'];
+            $NuevaCita->cliente_nombre =$arrayDatos['cliente_nombre'];
+            $NuevaCita->cliente_telefono =$arrayDatos['cliente_telefono'];
+            $NuevaCita->cliente_email =$arrayDatos['cliente_email'];
+            $NuevaCita->save();
+        
     	}
-      public function eliminar()
+      public function eliminar($id)
     	{
-      
+      		$Cita = App\cita::find($id);
+			if ($Cita === null) 
+				{
+   					return "cita no existe,verificar bien la codificacion";
+				}
+				else
+					{
+						$Cita->delete();
+					}
+			
     	}
-      public function editar()
+         /* 
+    * @param arrayDatos estructura con nombre,telefono,email
+    */
+      public function editar($cita_id,$arrayDatos)
     	{
-      
+              $Cita = App\cita::find($cita_id);
+        if ($Cita === null) 
+        {
+            return "cita no existe,no se pudo realizar la modificacion";
+        }
+        else
+          {
+            $Cita->cliente_nombre = $arrayDatos['cliente_nombre'];
+            $Cita->cliente_telefono =  $arrayDatos['cliente_telefono'];
+            $Cita->cliente_email =  $arrayDatos['cliente_email'];
+            $Cita->save();
+          }
     	}
-      public function reagendar()
+   /* 
+    * @param arrayDatos estructura con fecha y hora 
+    */
+      public function reagendar($cita_id,$arrayDatos)
     	{
+        $Cita = App\cita::find($cita_id);
+        if ($Cita === null) 
+        {
+            return "cita no existe,no se pudo realizar la modificacion";
+        }
+        else
+          {
+            $Cita->fecha = $arrayDatos['fecha'];
+            $Cita->hora =  $arrayDatos['hora'];
+            $Cita->save();
+          }
+       
       
     	}
       public function asignarTipo()
     	{
       
     	}
-      public function CambiarTipo()
+         /* 
+    * @param Dato variable numerica que hace relacion a un id de la tabla Tipo
+    */
+      public function CambiarTipo($cita_id,$Dato)
     	{
-      
+        $Cita = App\cita::find($cita_id);
+        if ($Cita === null) 
+        {
+            return "cita no existe,no se pudo realizar la modificacion";
+        }
+        else
+          {
+            $Cita->tipo_id = $Dato;
+            $Cita->save();
+          }
     	}    	
 }
--- app/tipo.php
+++ app/tipo.php
@@ -15,20 +15,45 @@ class tipo extends Model
         return $this->hasMany('App\cita');
     	}
     	//metodos de clase
-      public function agregar()
+          /* 
+    * @param arrayDatos estructura con el nombre y la duracion del tipo de cita 
+    */
+      public function agregar($arrayDatos)
     	{
+            $NuevoTipo = new App\tipo;
+            $NuevaCita->nombre =$arrayDatos['fecha'];
+            $NuevaCita->duracion =$arrayDatos['hora'];
+            $NuevaCita->save();
+    	}
+      public function eliminar($id)
+    	{
+            $Tipo = App\tipo::find($id);
+            if ($Tipo === null) 
+              {
+              return "cita no existe,verificar bien la codificacion";
+              }
+            else
+                {
+                $Tipo->delete();
+                }
       
     	}
-      public function eliminar()
+    /* 
+    * @param arrayDatos estructura con el nombre y la duracion del tipo de cita 
+    */
+      public function editar($arrayDatos,$tipo_id)
     	{
-      
+        $Tipo = App\cita::tipo($tipo_id);
+          if ($Tipo === null) 
+            {
+            return "cita no existe,no se pudo realizar la modificacion";
+            }
+          else
+            {
+              $Tipo->nombre =   $arrayDatos['nombre'];
+              $Tipo->duracion = $arrayDatos['duracion'];
+              $Cita->save();
+            }
     	}
-      public function editar()
-    	{
-      
-    	}
-      public function reagendar()
-    	{
-      
-    	}
+
 }
