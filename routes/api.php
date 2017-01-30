7d308707c1a7cf9298b1e3fd9530042f7b234755
leyven <nyhedgg@gmail.com>
2017-01-30 14:55:30 -0600

    fixed mistake

--- routes/api.php
+++ routes/api.php
@@ -13,95 +13,87 @@ use Illuminate\Http\Request;
 |
 */
 
-Route::group(['middleware' => ['cors'], 'prefix' => 'v1'],function(){
-
-   Route::post('login','AuthenticateController@authenticate');
-   Route::get('test_login','ValidacionController@authenticate');
-   Route::get('registro_test','Auth\RegisterController@createTest');
-   Route::get('servicio-disponible','CitaController@horasDisponibles');
-   Route::get('disponibilidad','CitaController@disponibilidadCalendario');
-   Route::get('sms','CitaController@sms');
-   Route::get('email','CitaController@mail');
-   Route::put('cita-r','CitaController@reagendar');
-
-
-
-   Route::get('images/{image_name}','ArticuloController@getImage');
-   Route::get('articulo/{id}','ArticuloController@getArticulo');
-
-      Route::get('/tipo','TipoController@index');
-
-
-   Route::get('testurl','CalendarioController@url');
-Route::get('obtener_horas','CitaController@filtrarHoras');
-   Route::get('dias_habiles','CalendarioController@getDiasHabiles');
-
-   Route::get('fecha_inhabil','CalendarioController@getDiasHorasInhabiles');
-
-   Route::get('algoritmo','CalendarioController@algoritmo');
-
-   Route::get('user_info','UsuarioController@getPerfilInfo');
-
-   Route::get('foto_perfil/{image_name}','UsuarioController@getProfilePicture');
+Route::group(['middleware' => ['cors'], 'prefix' => 'v1'], function () {
+    Route::post('login', 'AuthenticateController@authenticate');
+    Route::get('test_login', 'ValidacionController@authenticate');
+    Route::get('registro_test', 'Auth\RegisterController@createTest');
+    Route::get('servicio-disponible', 'CitaController@horasDisponibles');
+    Route::get('disponibilidad', 'CitaController@disponibilidadCalendario');
+    Route::get('sms', 'CitaController@sms');
+    Route::get('email', 'CitaController@mail');
+    Route::put('cita-r', 'CitaController@reagendar');
 
 
 
+    Route::get('images/{image_name}', 'ArticuloController@getImage');
+    Route::get('articulo/{id}', 'ArticuloController@getArticulo');
+
+    Route::get('/tipo', 'TipoController@index');
+
+
+    Route::get('testurl', 'CalendarioController@url');
+    Route::get('obtener_horas', 'CitaController@filtrarHoras');
+    Route::get('dias_habiles', 'CalendarioController@getDiasHabiles');
+
+    Route::get('fecha_inhabil', 'CalendarioController@getDiasHorasInhabiles');
+
+    Route::get('algoritmo', 'CalendarioController@algoritmo');
+
+    Route::get('user_info', 'UsuarioController@getPerfilInfo');
+
+    Route::get('foto_perfil/{image_name}', 'UsuarioController@getProfilePicture');
 });
 
-Route::group(['middleware' => ['cors','jwt.refresh'],'prefix' => 'v1'],function(){
-
-    Route::get('refresh_token',function(){
-      $oldtoken = JWTAuth::getToken();
-      $token = JWTAuth::refresh($oldtoken);
-      return response()->json(compact('token'));
-
+Route::group(['middleware' => ['cors','jwt.refresh'],'prefix' => 'v1'], function () {
+    Route::get('refresh_token', function () {
+        $oldtoken = JWTAuth::getToken();
+        $token = JWTAuth::refresh($oldtoken);
+        return response()->json(compact('token'));
     });
-
 });
 
 Route::group(['middleware' => ['cors','jwt.auth'], 'prefix' => 'v1'],
  function () {
-   Route::get('dashboard','CalendarioController@index');
+     Route::get('dashboard', 'CalendarioController@index');
    /*CRUD HORARIO*/
-   Route::get('horario','CalendarioController@asignar_horario');
+   Route::get('horario', 'CalendarioController@asignar_horario');
 
    /*FIN CRUD HORARIO */
-   Route::get('inhabilitar_fecha','CalendarioController@inhabilitar_fecha');
+   Route::get('inhabilitar_fecha', 'CalendarioController@inhabilitar_fecha');
 
    /*Crud Citas*/
 
-   Route::get('cita','CitaController@index');
-   Route::post('cita','CitaController@store');
-   Route::put('/cita/{id}','CitaController@update');
+   Route::get('cita', 'CitaController@index');
+     Route::post('cita', 'CitaController@store');
+     Route::put('/cita/{id}', 'CitaController@update');
    
-   Route::delete('/cita/{id}','CitaController@destroy');
+     Route::delete('/cita/{id}', 'CitaController@destroy');
    //tipo-cita
-   Route::get('/tipo/{id}','TipoController@show');
-   Route::post('/tipo','TipoController@store');
-   Route::put('/tipo','TipoController@update');
-   Route::delete('/tipo','TipoController@destroy');
+   Route::get('/tipo/{id}', 'TipoController@show');
+     Route::post('/tipo', 'TipoController@store');
+     Route::put('/tipo', 'TipoController@update');
+     Route::delete('/tipo', 'TipoController@destroy');
 
 
-   //Articulos 
+   //Articulos
 
-   Route::post('articulo','ArticuloController@store');
-   Route::get('articulos','ArticuloController@getArticulos');
+   Route::post('articulo', 'ArticuloController@store');
+     Route::get('articulos', 'ArticuloController@getArticulos');
 
 
    //dias habiles
-   Route::post('dias_habiles','CalendarioController@setDiasHabiles');
+   Route::post('dias_habiles', 'CalendarioController@setDiasHabiles');
 
-   Route::post('fecha_inhabil','CalendarioController@setDiasHorasInhabiles');
+     Route::post('fecha_inhabil', 'CalendarioController@setDiasHorasInhabiles');
 
-   Route::delete('fecha_inhabil','CalendarioController@deleteDiasHorasInhabiles');
+     Route::delete('fecha_inhabil', 'CalendarioController@deleteDiasHorasInhabiles');
 
-   Route::post('logout','UsuarioController@logout2');
+     Route::post('logout', 'UsuarioController@logout2');
 
-   Route::put('avatar','UsuarioController@updateAvatar');
+     Route::put('avatar', 'UsuarioController@updateAvatar');
 
-   //cupones 
+   //cupones
 
-   Route::post('cupon','CuponController@create');
-   Route::get('cupon','CuponController@index');
-   
+   Route::post('cupon', 'CuponController@create');
+     Route::get('cupon', 'CuponController@index');
  });
