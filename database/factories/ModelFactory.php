2fed07f9afaa322bce7be5bbb88db91e13f14590
leyven <nyhedgg@gmail.com>
2017-02-08 16:09:39 -0600

    algunas pruebas implementadas
    timeslot,disponibilidadcal,fechadisponible,espaciosPorFecha

--- database/factories/ModelFactory.php
+++ database/factories/ModelFactory.php
@@ -1,4 +1,5 @@
 <?php
+use Carbon\Carbon;
 
 /*
 |--------------------------------------------------------------------------
@@ -20,5 +21,31 @@ $factory->define(App\User::class, function (Faker\Generator $faker) {
         'email' => $faker->unique()->safeEmail,
         'password' => $password ?: $password = bcrypt('secret'),
         'remember_token' => str_random(10),
+    ];
+});
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+$factory->define(App\tipo::class, function (Faker\Generator $faker) {
+
+	return[
+		'calendario_id'=> 1,
+		'nombre'=> $faker->company,
+		'duracion'=>  $faker->randomNumber($nbDigits = 2),
+		'costo'=>  $faker->randomNumber($nbDigits = 4),
+		'denominacion'=> 'MXN',
+	];
+});
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+$factory->define('App\cita', function(Faker\Generator $faker) {
+    $startDate = Carbon::createFromTimeStamp($faker->dateTimeBetween('now', '2 years')->getTimestamp());
+    $endDate = Carbon::createFromFormat('Y-m-d H:i:s', $startDate)->addHour();
+
+    return [
+       	'calendario_id'=> 1,
+		'tipo_id' => 1,
+		'fecha_inicio'=>  $startDate,
+		'fecha_final'=>  $endDate,
+		'cliente_nombre'=> $faker->name,
+		'cliente_telefono'=> $faker->randomNumber($nbDigits = 8),
+		'cliente_email'=> $faker->email,
     ];
 });
