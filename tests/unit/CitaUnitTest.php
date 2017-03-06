5ff67e2f34fd3299e23e95fa4315a2018b771daa
leyven <nyhedgg@gmail.com>
2017-03-06 14:48:53 -0600

    bug arreglado en timeslot + añadidos nuevos test
    :)

--- tests/unit/CitaUnitTest.php
+++ tests/unit/CitaUnitTest.php
@@ -11,7 +11,7 @@ use App\fechahora_inhabil;
 
 class CitaUnitTest extends TestCase
 {
-    use DatabaseTransactions;       
+    use DatabaseTransactions;
     
     public function fecha_esta_disponible_para_agendar()
     {
@@ -49,51 +49,77 @@ class CitaUnitTest extends TestCase
         $this->assertEquals($disponibilidadAlta, 1); //regresa disponibilidad Alta
     }
  
-        public function horas_del_dia()
+    public function horas_del_dia()
     {
         ///24 hrs day
         $fecha='2017-11-11';
         $calendario_id=3;
-        $horas=cita::horasDelDia($fecha,$calendario_id);
-        $this->assertInternalType('array',$horas);
-         $this->assertEquals(24,count($horas));
+        $horas=cita::horasDelDia($fecha, $calendario_id);
+        $this->assertInternalType('array', $horas);
+        $this->assertEquals(24, count($horas));
     }
            
-        public function horas_inhabiles_del_dia()
+    public function horas_inhabiles_del_dia()
     {
         ///dia inhabil completo
 
         $fecha='2017-03-24';
         $calendario_id=2;
-        $horas=cita::filtroHorasInhabiles($fecha,$calendario_id);
-        $this->assertInternalType('array',$horas);
-        $this->assertEquals(24,count($horas),json_encode($horas));
+        $horas=cita::filtroHorasInhabiles($fecha, $calendario_id);
+        $this->assertInternalType('array', $horas);
+        $this->assertEquals(24, count($horas), json_encode($horas));
         ///10 horas menos
         
         $fecha='2017-03-31';
         $calendario_id=2;
-        $horas=cita::filtroHorasInhabiles($fecha,$calendario_id);
-        $this->assertInternalType('array',$horas);
+        $horas=cita::filtroHorasInhabiles($fecha, $calendario_id);
+        $this->assertInternalType('array', $horas);
 
-        $this->assertEquals(10,count($horas),json_encode($horas));
-
+        $this->assertEquals(10, count($horas), json_encode($horas));
     }
     /** @test */
     public function dias_no_habiles_del_calendario()
     {
         $calendario_id=2;
         $horas=cita::diasNoHabiles($calendario_id);
-        $this->assertInternalType('array',$horas);
+        $this->assertInternalType('array', $horas);
 
-        $this->assertEquals(2,count($horas),json_encode($horas));
+        $this->assertEquals(2, count($horas), json_encode($horas));
     }
-     /** @test */
+
     public function revisar_si_la_fecha_esta_inhabilitada()
     {
         $datoscita['fecha_inicio']='2017-03-24';
         $datoscita['calendario_id']=2;
         $dia=cita::revisarDiasInhabiles($datoscita);
         $this->assertFalse($dia);
-        
+    }
+        /** @test */
+    public function verificar_funcionamiento_de_timeslot()
+    {
+        ////todo esto es asumiendo un dia de 9 horas
+        $fecha='2017-07-24';
+        ///60 mins
+        $tipo_id=1;
+        $calendario_id=2;
+        $horasdeldia=cita::timeslot($fecha, $tipo_id, $calendario_id);
+        $this->assertEquals(9, count($horasdeldia), json_encode($horasdeldia));
+         /////horas del dia
+        $fecha='2017-07-24';
+        ///120 mins
+        $tipo_id=2;
+        $calendario_id=2;
+        $horasdeldia=cita::timeslot($fecha, $tipo_id, $calendario_id);
+        $this->assertEquals(4, count($horasdeldia), json_encode($horasdeldia));
+                ///20 mins
+        $tipo_id=3;
+        $calendario_id=2;
+        $horasdeldia=cita::timeslot($fecha, $tipo_id, $calendario_id);
+        $this->assertEquals(27, count($horasdeldia), json_encode($horasdeldia));
+        ///1 min
+        $tipo_id=15;
+        $calendario_id=2;
+        $horasdeldia=cita::timeslot($fecha, $tipo_id, $calendario_id);
+        $this->assertEquals(540, count($horasdeldia), json_encode($horasdeldia));
     }
 }
