381541872650bccc4a5af4f1de32122e9e1624b3
leyven <nyhedgg@gmail.com>
2017-03-20 14:17:56 -0600

    .
    .

--- config/mail.php
+++ config/mail.php
@@ -56,7 +56,7 @@ return [
     */
 
     'from' => [
-        'address' => env('MAIL_FROM_ADDRESS', 'nyhedgg@gmail.com'),
+        'address' => env('MAIL_FROM_ADDRESS', 'ContactoClientes@creatver.com'),
         'name' => env('MAIL_FROM_NAME', 'ContactoClientes'),
     ],
 
