e2b3f5adcc1e9e38ce9ad8fe911ce2a876d601ba
Cristian Michel <cristianrocker93@gmail.com>
2017-03-29 12:29:03 -0600

    Articulo resolve url solved 2

--- resources/views/resolve_articulo.blade.php
+++ resources/views/resolve_articulo.blade.php
@@ -3,13 +3,12 @@
 <head>
 <meta charset="UTF-8">
 @if(isset($articulo))
-	<meta property="og:url" content="www.creatver.com/sistaarticulo/sistacrimi/{{$articulo->id}}">
+	<meta property="og:url" content="https://www.creatver.com/sistaarticulo/sistacrimi/{{$articulo->id}}">
+	<meta property="og:type"   content="article" />
 	<meta property="og:title" content="{{$articulo->titulo}}">
-	<meta property="og:image" content="{{$articulo->caratula}}">
-	<meta property="og:image:secure_url" content="{{$articulo->caratula}}">
-	<meta property="og:image:type" content="image/EXT">
 	<meta property="og:description" content="{{$articulo->resumen}}">
-	<meta property="og:type" content="article"/>
+	<meta property="og:image" content="{{$articulo->caratula}}">
+
 @endif
 
 </head>
