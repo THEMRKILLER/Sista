d24e298fb131279d7ba6cec93a2e7053d70dcaff
Cristian Michel <cristianrocker93@gmail.com>
2017-03-30 14:34:18 -0600

    rutas

--- resources/views/resolve_articulo.blade.php
+++ resources/views/resolve_articulo.blade.php
@@ -3,7 +3,7 @@
 <head>
 <meta charset="UTF-8">
 @if(isset($articulo))
-	<meta property="og:url" content="https://www.creatver.com/sistaarticulo/sistacrimi/{{$articulo->id}}">
+	<meta property="og:url" content="https://www.creatver.com/sistaarticulo/sistacrimi/articulo/{{$articulo->id}}">
 	<meta property="og:type"   content="article" />
 	<meta property="og:title" content="{{$articulo->titulo}}">
 	<meta property="og:description" content="{{$articulo->resumen}}">
@@ -16,7 +16,7 @@
 <body>
 </body>
 <script>
- window.location.assign("https://www.creatver.com/sistaarticulo/sistacrimi/{{$articulo->id}}");
+ window.location.assign("https://www.creatver.com/sistaarticulo/sistacrimi/articulo/{{$articulo->id}}");
 
 </script>
 
