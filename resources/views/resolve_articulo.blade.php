2ee0798979b888f2b23fa3b1d5ca8fe616f1e77e
Cristian Michel <cristianrocker93@gmail.com>
2017-03-29 22:38:48 -0600

    okay art

--- resources/views/resolve_articulo.blade.php
+++ resources/views/resolve_articulo.blade.php
@@ -3,7 +3,7 @@
 <head>
 <meta charset="UTF-8">
 @if(isset($articulo))
-	<meta property="og:url" content="https://www.creatver.com/sistaarticulo/sistacrimi/{{$articulo->id}}">
+	
 	<meta property="og:type"   content="article" />
 	<meta property="og:title" content="{{$articulo->titulo}}">
 	<meta property="og:description" content="{{$articulo->resumen}}">
