a0891f5661b6cc9518cab6a69c52dd0f82b4abff
leyven <nyhedgg@gmail.com>
2017-04-17 14:38:48 -0500

    dynamic dates

--- app/Http/Controllers/PdfController.php
+++ app/Http/Controllers/PdfController.php
@@ -6,10 +6,12 @@ use Illuminate\Http\Request;
 use App\cita;
 class PdfController extends Controller
 {
-    public function CitasPorMes()
+    public function CitasPorMes(Request $request)
     {	
-    	$fecha1='2017-04-17';
-    	$fecha2='2017-04-30';
+    	
+    	
+    	$fecha1=$request->get('fecha1');
+    	$fecha2=$request->get('fecha2');
     	$data= cita::CitasXLapso($fecha1,$fecha2);
     	$view =  \View::make('PDF.CitasTemplate', compact('data'))->render();
         $pdf = \App::make('dompdf.wrapper');
